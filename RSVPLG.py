77906aa72e335d21cd43d687519f724d944ca03b
David Fencsik <david.fencsik@gmail.com>
2023-08-23 18:01:52 -0700

    Add warmup trials to each experimental block
    Runs a set of warmup trials at the beginning of experimental, but not
    practice, blocks. Add an additional loop to allow for these trials. Set up
    logic to quit after the appropriate number of trials have been run, and
    also add logic to quit after the desired number of practice trials on
    practice blocks.

--- RSVPLG.py
+++ RSVPLG.py
@@ -13,8 +13,9 @@ t2_letters = 'X'
 t1_pos_list = [3, 4, 5, 6]
 stim_size = [204, 238]
 stim_file_ext = 'jpg'
-n_trials_per_cell = 1 # experimental blocks
-n_trials_practice = 10
+n_trials_per_cell = 1 # for exp blocks
+n_trials_practice = 10 # of trials in prac blocks
+n_trials_warmup = 5 # of warmup trials in exp blocks
 conditions_file = 'RSVPLGTrials.csv'
 feedback_color_correct = 'green'
 feedback_color_error = 'red'
@@ -273,10 +274,13 @@ extraInfo = {
 test_t1 = True
 if BLOCK_TYPE == 'Practice1':
     test_t2 = False
+    trial_type_list = ['exp']
 elif BLOCK_TYPE == 'Practice2':
     test_t2 = True
+    trial_type_list = ['exp']
 else:
     test_t2 = True
+    trial_type_list = ['warmup', 'exp']
 
 # open experiment handler
 exp_handler = data.ExperimentHandler(name=EXPERIMENT, version=VERSION,
@@ -284,12 +288,16 @@ exp_handler = data.ExperimentHandler(name=EXPERIMENT, version=VERSION,
                                      saveWideText=True,
                                      dataFileName=data_file_basename)
 
-# set up trial handler
-trial_handler = data.TrialHandler(
-    data.importConditions(conditions_file),
+# set up trial handlers
+conditions_list = data.importConditions(conditions_file)
+warmup_trial_handler = data.TrialHandler(
+    conditions_list,
+    nReps=np.ceil(n_trials_warmup / len(conditions_list)),
+    method='fullRandom')
+exp_trial_handler = data.TrialHandler(
+    conditions_list,
     nReps=n_trials_per_cell,
     method='fullRandom')
-exp_handler.addLoop(trial_handler)
 
 # set up stimuli and responses
 distractor_letters = list(distractor_letters)
@@ -347,152 +355,163 @@ t2_response_prompt = visual.TextBox2(
 t2_response_prompt.setAutoDraw(False)
 
 trial = 0
-for thisTrial in trial_handler:
-    trial += 1
-    # set factor levels for this trial
-    t1_level = thisTrial['t1_level']
-    t2_level = thisTrial['t2_level']
-    t2_lag = int(thisTrial['t2_lag'])
+for trial_type in trial_type_list:
+    exec('trial_handler = {}_trial_handler'.format(trial_type))
+    exp_handler.addLoop(trial_handler)
+    for thisTrial in trial_handler:
+        trial += 1
+        trial_handler.addData('trial', trial)
+        trial_handler.addData('trial_type', trial_type)
+        # set factor levels for this trial
+        t1_level = thisTrial['t1_level']
+        t2_level = thisTrial['t2_level']
+        t2_lag = int(thisTrial['t2_lag'])
 
-    if type(rsvp_stream_frames) in (list, tuple):
-        n_frames = rng.choice(rsvp_stream_frames)
-    else:
-        n_frames = rsvp_stream_frames
-
-    # set up stimuli
-    t1_pos = rng.choice(t1_pos_list)
-    # select t1 from distractors, making sure to get 2 different letters
-    t1_list = rng.choice(distractor_letters, 2, replace=False)
-    t1_global = t1_list[0]
-    t1_local = t1_list[1]
-    # remove t1 letters from rest of stream
-    trial_letters = np.array(distractor_letters)
-    trial_letters = trial_letters[
-        (trial_letters != t1_global) & (trial_letters != t1_local)]
-    # extend letters if needed
-    if len(trial_letters) < n_frames:
-        trial_letters = np.tile(
-            trial_letters, math.ceil(n_frames / len(trial_letters)))
-    global_letters = rng.permutation(trial_letters)[:n_frames]
-    local_letters = rng.permutation(trial_letters)[:n_frames]
-    # insert T1
-    global_letters[t1_pos - 1] = t1_global
-    local_letters[t1_pos - 1] = t1_local
-    # set up colors
-    stream_colors = ['black'] * n_frames
-    stream_colors[t1_pos - 1] = 'white'
-    # store stimuli and responses
-    if t1_level == 'global':
-        t1 = t1_global
-    else:
-        t1 = t1_local
-    t1_correct_resp = [t1.lower(), t1]
-    if t2_lag > 0:
-        t2 = rng.choice(t2_letters)
-        t2_correct_resp = list('yY')
-        if t2_level == 'global':
-            global_letters[t1_pos + t2_lag - 1] = t2
+        if type(rsvp_stream_frames) in (list, tuple):
+            n_frames = rng.choice(rsvp_stream_frames)
         else:
-            local_letters[t1_pos + t2_lag - 1] = t2
-    else:
-        t2 = 'absent'
-        t2_correct_resp = list('nN')
-    trial_handler.addData('global_letters', ''.join(global_letters))
-    trial_handler.addData('local_letters', ''.join(local_letters))
-    trial_handler.addData('t1_pos', t1_pos)
-    trial_handler.addData('t1', t1)
-    trial_handler.addData('t2', t2)
-    trial_handler.addData('t1_corr', ''.join(t1_correct_resp))
-    trial_handler.addData('t2_corr', ''.join(t2_correct_resp))
+            n_frames = rsvp_stream_frames
 
-    rsvp_stream.initializeStream(global_letters, local_letters, stream_colors)
+        # set up stimuli
+        t1_pos = rng.choice(t1_pos_list)
+        # select t1 from distractors, making sure to get 2 different letters
+        t1_list = rng.choice(distractor_letters, 2, replace=False)
+        t1_global = t1_list[0]
+        t1_local = t1_list[1]
+        # remove t1 letters from rest of stream
+        trial_letters = np.array(distractor_letters)
+        trial_letters = trial_letters[
+            (trial_letters != t1_global) & (trial_letters != t1_local)]
+        # extend letters if needed
+        if len(trial_letters) < n_frames:
+            trial_letters = np.tile(
+                trial_letters, math.ceil(n_frames / len(trial_letters)))
+        global_letters = rng.permutation(trial_letters)[:n_frames]
+        local_letters = rng.permutation(trial_letters)[:n_frames]
+        # insert T1
+        global_letters[t1_pos - 1] = t1_global
+        local_letters[t1_pos - 1] = t1_local
+        # set up colors
+        stream_colors = ['black'] * n_frames
+        stream_colors[t1_pos - 1] = 'white'
+        # store stimuli and responses
+        if t1_level == 'global':
+            t1 = t1_global
+        else:
+            t1 = t1_local
+        t1_correct_resp = [t1.lower(), t1]
+        if t2_lag > 0:
+            t2 = rng.choice(t2_letters)
+            t2_correct_resp = list('yY')
+            if t2_level == 'global':
+                global_letters[t1_pos + t2_lag - 1] = t2
+            else:
+                local_letters[t1_pos + t2_lag - 1] = t2
+        else:
+            t2 = 'absent'
+            t2_correct_resp = list('nN')
+        trial_handler.addData('global_letters', ''.join(global_letters))
+        trial_handler.addData('local_letters', ''.join(local_letters))
+        trial_handler.addData('t1_pos', t1_pos)
+        trial_handler.addData('t1', t1)
+        trial_handler.addData('t2', t2)
+        trial_handler.addData('t1_corr', ''.join(t1_correct_resp))
+        trial_handler.addData('t2_corr', ''.join(t2_correct_resp))
 
-    # load stimuli and do pre-trial pause
-    rsvp_stream.preLoadStream(clear=True)
-    win.flip()
-    core.wait(dur['pre_trial'])
+        rsvp_stream.initializeStream(global_letters, local_letters, stream_colors)
 
-    # draw cue
-    win.clearBuffer()
-    trial_cue.draw(t1_level)
-    win.flip()
-    core.wait(dur['cue'])
-
-    # draw fixation
-    win.clearBuffer()
-    fixation.draw()
-    win.flip()
-    core.wait(dur['fixation'])
-
-    # RSVP stream
-    while True:
-        win.clearBuffer()
-        rsvp_stream.drawCurrent()
-        core.wait(dur['isi']) # isi
-        t = win.flip()
-        win.clearBuffer()
-        core.wait(dur['stim']) # stim dur
+        # load stimuli and do pre-trial pause
+        rsvp_stream.preLoadStream(clear=True)
         win.flip()
-        if not rsvp_stream.nextFrame():
+        core.wait(dur['pre_trial'])
+
+        # draw cue
+        win.clearBuffer()
+        trial_cue.draw(t1_level)
+        win.flip()
+        core.wait(dur['cue'])
+
+        # draw fixation
+        win.clearBuffer()
+        fixation.draw()
+        win.flip()
+        core.wait(dur['fixation'])
+
+        # RSVP stream
+        while True:
+            win.clearBuffer()
+            rsvp_stream.drawCurrent()
+            core.wait(dur['isi']) # isi
+            t = win.flip()
+            win.clearBuffer()
+            core.wait(dur['stim']) # stim dur
+            win.flip()
+            if not rsvp_stream.nextFrame():
+                break
+
+        # pause before response collection
+        core.wait(dur['response_gaps'])
+        win.clearBuffer()
+
+        if test_t1:
+            # T1 response
+            t1_response_prompt.draw()
+            win.flip()
+            keyboard.clock.reset()
+            keys = keyboard.waitKeys(keyList=t1_allowed_responses)
+            t1_response_dict = ProcessResponse(
+                keys, t1_correct_resp, t1_allowed_responses)
+            # pause
+            core.wait(dur['response_gaps'])
+            win.clearBuffer()
+        else:
+            t1_response_dict = ProcessResponse(None)
+
+        if test_t2:
+            # T2 response
+            t2_response_prompt.draw()
+            win.flip()
+            keyboard.clock.reset()
+            keys = keyboard.waitKeys(keyList=t2_allowed_responses)
+            t2_response_dict = ProcessResponse(
+                keys, t2_correct_resp, t2_allowed_responses)
+            # pause
+            core.wait(dur['response_gaps'])
+            win.clearBuffer()
+        else:
+            t2_response_dict = ProcessResponse(None)
+
+        trial_handler.addData('t1_resp', t1_response_dict['resp'])
+        trial_handler.addData('t1_acc', t1_response_dict['acc'])
+        trial_handler.addData('t1_rt', t1_response_dict['rt'])
+        trial_handler.addData('t2_resp', t2_response_dict['resp'])
+        trial_handler.addData('t2_acc', t2_response_dict['acc'])
+        trial_handler.addData('t2_rt', t2_response_dict['rt'])
+
+        # feedback
+        feedback.prepare(trial,
+            t1AccText=t1_response_dict['fdbk'],
+            t1Color=t1_response_dict['fdbk_color'],
+            t2AccText=t2_response_dict['fdbk'],
+            t2Color=t2_response_dict['fdbk_color'])
+        win.clearBuffer()
+        feedback.draw()
+        win.flip()
+        core.wait(dur['feedback'])
+
+        # post-trial pause
+        win.clearBuffer()
+        win.flip()
+        core.wait(dur['post_trial'])
+
+        # advance trials
+        exp_handler.nextEntry()
+        if ((trial_type == 'warmup' and trial >= n_trials_warmup) or
+            (BLOCK_TYPE in ['Practice1', 'Practice2'] and trial >= n_trials_practice)):
+            # end warmup
             break
 
-    # pause before response collection
-    core.wait(dur['response_gaps'])
-    win.clearBuffer()
-
-    if test_t1:
-        # T1 response
-        t1_response_prompt.draw()
-        win.flip()
-        keyboard.clock.reset()
-        keys = keyboard.waitKeys(keyList=t1_allowed_responses)
-        t1_response_dict = ProcessResponse(
-            keys, t1_correct_resp, t1_allowed_responses)
-        # pause
-        core.wait(dur['response_gaps'])
-        win.clearBuffer()
-    else:
-        t1_response_dict = ProcessResponse(None)
-
-    if test_t2:
-        # T2 response
-        t2_response_prompt.draw()
-        win.flip()
-        keyboard.clock.reset()
-        keys = keyboard.waitKeys(keyList=t2_allowed_responses)
-        t2_response_dict = ProcessResponse(
-            keys, t2_correct_resp, t2_allowed_responses)
-        # pause
-        core.wait(dur['response_gaps'])
-        win.clearBuffer()
-    else:
-        t2_response_dict = ProcessResponse(None)
-
-    trial_handler.addData('t1_resp', t1_response_dict['resp'])
-    trial_handler.addData('t1_acc', t1_response_dict['acc'])
-    trial_handler.addData('t1_rt', t1_response_dict['rt'])
-    trial_handler.addData('t2_resp', t2_response_dict['resp'])
-    trial_handler.addData('t2_acc', t2_response_dict['acc'])
-    trial_handler.addData('t2_rt', t2_response_dict['rt'])
-
-    # feedback
-    feedback.prepare(trial,
-        t1AccText=t1_response_dict['fdbk'],
-        t1Color=t1_response_dict['fdbk_color'],
-        t2AccText=t2_response_dict['fdbk'],
-        t2Color=t2_response_dict['fdbk_color'])
-    win.clearBuffer()
-    feedback.draw()
-    win.flip()
-    core.wait(dur['feedback'])
-
-    # post-trial pause
-    win.clearBuffer()
-    win.flip()
-    core.wait(dur['post_trial'])
-
-    # advance trials
-    exp_handler.nextEntry()
+    exp_handler.loopEnded(trial_handler)
 
 exp_handler.saveAsWideText(data_file_basename, delim=',')
 exp_handler.abort()
