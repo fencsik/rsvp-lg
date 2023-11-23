66a621a519fc039b4ba36985ca51f9e31197fb77
David Fencsik <fencsik@gmail.com>
2023-11-23 14:08:52 -0800

    Correct two participant IDs and output before and after summaries

--- rsvplg01/clean_data.py
+++ rsvplg01/clean_data.py
@@ -8,5 +8,10 @@ pd.set_option('display.max_rows', None)
 infile = 'alldata.csv'
 
 df = pd.read_csv(infile)
-df = df[df['trial_type'] == 'exp']
-print(df['trial'].groupby([df['sub'], df['blocktyp']]).count().unstack())
+print(df.dtypes)
+df2 = df[df['trial_type'] == 'exp']
+print(df2['trial'].groupby([df2['sub'], df2['blocktyp']]).count().unstack())
+df.loc[(df['sub'] == 317) & (df['experimenter'] == 'SN'), 'sub'] = 137
+df.loc[(df['sub'] == 514) & (df['experimenter'] == 'AGM'), 'sub'] = 154
+df2 = df[df['trial_type'] == 'exp']
+print(df2['trial'].groupby([df2['sub'], df2['blocktyp']]).count().unstack())
